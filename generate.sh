49abe6017e543fb465fd5e4d897a19c528938ebd
Pedro <pedro.araoz@avioconsulting.com>
2024-03-26 14:46:39 -0300

    added comments to generate.sh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,8 @@
-npm run generate \
-&& rm -rf ../apm \
-&& cp -r dist/apm ../apm \
+`# Runs project generation`
+npm run generate `# Removes apm folder from parent directory` \
+&& rm -rf ../apm `# Copies newly generated apm folder to parent directory` \
+&& cp -r dist/apm ../apm `# Creates temp file from model_deployment_status_type.go removing one line` `# This line is removed because it creates a conflict with another global variable named DRAFT`\
 && grep -v "DRAFT DeploymentStatusType = \"DRAFT\"" ../apm/model_deployment_status_type.go > temp \
-&& mv temp ../apm/model_deployment_status_type.go \
+&& mv temp ../apm/model_deployment_status_type.go `# Replaces model_deployment_status_type.go with temp file` \
 && echo "Finished creating new go-client"
+
