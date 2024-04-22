46159115f850a7c22519cc98b67e3c84f87f7354
Tanner Sherman <tanner.sherman@avioconsulting.com>
2024-04-22 04:59:19 -0400

    Made adjustments to add go.mod and go.sum to auto generate in /apm directory so this package can be imported into other projects Updated generate.sh to work for local testing and this repository

--- generate.sh
+++ generate.sh
@@ -1,8 +1,19 @@
-`# Runs project generation`
-npm run generate \
-&& rm -rf ../apm `# Removes apm folder from parent directory` \
-&& cp -r dist/apm ../apm `# Copies newly generated apm folder to parent directory` \
-&& grep -v "DRAFT DeploymentStatusType = \"DRAFT\"" ../apm/model_deployment_status_type.go > temp `# Creates temp file from model_deployment_status_type.go removing one line` `# This line is removed because it creates a conflict with another global variable named DRAFT` \
-&& mv temp ../apm/model_deployment_status_type.go `# Replaces model_deployment_status_type.go with temp file` \
-&& echo "Finished creating new go-client"
+# Runs project generation and removes incorrect file that references DRAFT in model_deployment_status_type.go. Used to install locally and in this repository
+npm run generate
+# Removes apm folder from parent directory
+rm -rf ../apm 
+# Copies newly generated apm folder to parent directory for local testing
+cp -r apm ../apm
+# Creates temp file from model_deployment_status_type.go removing one line. This line is removed because it creates a conflict with another global variable named DRAFT
+grep -v "DRAFT DeploymentStatusType = \"DRAFT\"" ../apm/model_deployment_status_type.go > temp
+# Remove generated files using DRAFT line
+rm ../apm/model_deployment_status_type.go
+rm apm/model_deployment_status_type.go
+# Copy temp file to model_deployment_status_type.go w/o the DRAFT line then remove 
+cp temp ../apm/model_deployment_status_type.go
+cp temp apm/model_deployment_status_type.go
+rm temp
+cd apm/
+go mod tidy
+echo "Finished creating new go-client"
 
